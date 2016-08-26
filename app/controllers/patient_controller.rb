87440672f673dba9708972e1977a56e5dd42cad4
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-08-26 11:57:12 -0700

    user controller and test views/formtastic

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,2 +1,17 @@
 class PatientController < ApplicationController
+
+  def show
+    @patient.find_by_id(params[:id])
+  end
+
+  def edit
+    @patient.find_by_id(params[:id])
+  end
+
+  def update
+    patient_params = params.require(:patient).permit(:date_of_birth, :photo, :location)
+    @patient.find_by_id(params[:id])
+    @patient.update_attributes(patient_params)
+  end
+
 end
