3be12e539f5bf220173328e185359ebf1a29aa04
tbduong <tiffaniduong@gmail.com>
2016-08-27 14:10:13 -0700

    caregiver index and change in sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :patient)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :patient)
   end
 
 end
