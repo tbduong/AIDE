0ecb5e188e1a4bfd9af89774e1904a4f21c0f39b
tbduong <tiffaniduong@gmail.com>
2016-08-28 21:41:01 -0700

    date_of_birth datatype change failed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827230128) do
+ActiveRecord::Schema.define(version: 20160826204836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20160827230128) do
     t.integer  "user_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.text     "bio"
     t.index ["user_id"], name: "index_patients_on_user_id", using: :btree
   end
 
