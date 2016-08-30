ccb0b1a441e654a663008679ec88aa7e31f3127f
SwittS <switt.srikulacheep@gmail.com>
2016-08-30 10:33:30 -0700

    add photo to caregivers index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
 
   def image_tag(img , options={})
-  path = "#{Rails.root}/app/assets/images/#{img}"
-  img = "default_profile.png"  unless img.present?
-  super(img, options)
-end
-
+    path = "#{Rails.root}/app/assets/images/#{img}"
+    img = "default_profile.png"  unless img.present?
+    p super(img, options)
+  end
 end
