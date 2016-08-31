3121dbd5ae13de37e1ab56964dd63fbf72f0cb74
sophn11 <sophianakpil@gmail.com>
2016-08-31 12:34:02 -0700

    added pretty urls and starting friendly_id - attempted to seed multiple caregivers under specialties but could not fix

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,4 +1,8 @@
 class Specialty < ApplicationRecord
   has_many :caregiver_specialties
   has_many :caregivers, through: :caregiver_specialties
+
+  def to_param
+    "#{id} #{skill}".parameterize
+  end
 end
