390766e6dcb7ccec3bbaccf640a96e5f1d4794e6
sophn11 <sophianakpil@gmail.com>
2016-08-31 00:58:16 -0700

    ran beutify, removed specialty from caregiver update params, attempting edit on specialties

--- app/controllers/caregivers_controller.rb
+++ app/controllers/caregivers_controller.rb
@@ -28,7 +28,7 @@ class CaregiversController < ApplicationController
   end
 
   def update
-    caregiver_params = params.require(:caregiver).permit(:experience, :file, :location, :credentials, :specialty)
+    caregiver_params = params.require(:caregiver).permit(:experience, :file, :location, :credentials)
     @caregiver = Caregiver.find_by_id(params[:id])
     if @caregiver.update(caregiver_params)
       redirect_to caregiver_path(@caregiver)
--- app/controllers/specialties_controller.rb
+++ app/controllers/specialties_controller.rb
@@ -8,4 +8,22 @@ class SpecialtiesController < ApplicationController
     @specialty = Specialty.find_by_id(params[:id])
   end
 
+  def edit
+    @specialty = Specialty.find_by_id(params[:id])
+    if session[:user_id] != @patient[:user_id]
+      flash[:error] = "You are NOT authorized to edit this profile."
+      redirect_to '/'
+    end
+  end
+
+  def update
+    specialty_params = params.require(:specialty).permit(:skill)
+    @specialty = Caregiver.find_by_id(params[:id])
+    if @specialty.update(specialty_params)
+      redirect_to specialty_path(@specialty)
+    else
+      redirect_to :back
+    end
+  end
+
 end
