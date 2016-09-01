952411a88587ba5eefa4e49d87d83f7f11cb20f3
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-09-01 08:44:12 -0700

    semi functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
   include SessionsHelper
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+  
 end
